7bd18169f5e1c367c0e3af495bdb4d4a38985bcf
Eric Wang <echwang@live.com>
2015-11-02 20:20:20 -0800

    incorrect check-in for bubble sort

--- bubble sort.py
+++ bubble sort.py
@@ -1,16 +1,23 @@
 import random
+import datetime
 def bubbleSort(list):
-	# print "length = " + str(len(list))
-	for x in range(0,len(list)-1):
-		for y in range(x+1, len(list)):
-			if list[x] > list[y]:
-				temp = list[x]
-				list[x] = list[y]
-				list[y] = temp
+	sort = True
+
+	while sort:
+		sort = False
+		for x in range(len(list)-1):
+			if list[x] > list[x+1]:
+				sort = True
+				list[x], list[x+1] = list[x+1], list[x]
+	
 
 #main execution
-# x = [1,90, 24, 88, 98, 2, 7]
-x=[random.randint(0,10000) for p in range(0,100)]
+x=[random.randint(0,10000) for p in range(0,10)]
 print "Original x = " + str(x)
+t1 = datetime.datetime.now()
 bubbleSort(x)
+t2 = datetime.datetime.now()
+tdelta = t2 - t1
 print "New x = " + str(x)
+
+print "Bubble sort took " + str(tdelta)
